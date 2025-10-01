bdefaf83d69b34d007642eea9f3263fb81aed3c9
chandu_116 <118456799+chandra71@users.noreply.github.com>
2025-10-01 23:54:34 +0530

    Add files via upload

---
+++ Javascript part2/Nonprimitives.js
@@ -0,0 +1,25 @@
+//Objects
+
+let username = {
+  firstname: "chandu",
+  isLoggedin: true,
+};
+
+username.firstname = "Mr";
+
+console.log(username.firstname);
+console.log(typeof username);
+
+let today = new Date();
+console.log(today.getDate());
+
+// Arrays
+
+let anotheruser = ["Chandu", true];
+
+console.log(anotheruser[0]);
+
+let isvalue = "2abc";
+
+console.log(typeof Number(isvalue));
+console.log(null);
